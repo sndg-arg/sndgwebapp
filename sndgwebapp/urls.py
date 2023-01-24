adc217463476e9a355a2a87f19e4031c9dea716e
Ezequiel <ezequieljsosa@gmail.com>
2023-01-24 16:06:17 -0300

    test para beanstalk

--- sndgwebapp/urls.py
+++ sndgwebapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.MainPage import main_page
-from .views.TestCelery import test_celery
+#from .views.TestCelery import test_celery
+
 from sndgwebapp.views.UserViews import (
     user_detail_view,
     user_redirect_view,
@@ -15,6 +16,6 @@ urlpatterns = [
     #path("~redirect/", view=user_redirect_view, name="redirect"),
     #path("~update/", view=user_update_view, name="update"),
     #path("<str:username>/", view=user_detail_view, name="detail"),
-    path("test_celery/", view=test_celery, name="test_celery"),
+    #path("test_celery/", view=test_celery, name="test_celery"),
     path("",view=main_page)
 ]
